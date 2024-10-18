46928d1ae0d8286b39afcd1aecfa3cbb33d0038d
whatIsLove <vlad.galyara@gmail.com>
2024-10-18 12:03:11 +0300

    feat(UserService): implement Eureka configuration and actuator endpoints

--- UserService/src/main/java/com/cr1stal423/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/cr1stal423/userservice/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.cr1stal423.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
